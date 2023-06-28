51b5f34b98413507d6d7e5daf97f36a5066ed933
DillDogG <dilldoggaming@gmail.com>
2023-06-28 12:13:55 -0600

    Add project files.

---
+++ Source/Game/Game/Main.cpp
@@ -0,0 +1,18 @@
+#include "Core/Random.h"
+#include "Core/FileIO.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << kiko::getFilePath() << endl;
+
+
+	kiko::seedRandom((unsigned int)time(nullptr));
+	for (int i = 0; i < 3; i++)
+	{
+		cout << kiko::random(5, 10) << endl;
+	}
+}
